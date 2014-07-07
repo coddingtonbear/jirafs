e164ffc4576c965385a6b152bcbace32a9dade21
Adam Coddington <adam.coddington@coxinc.com>
2014-07-07 15:09:15 -0700

    Fleshing out a little more internal structure.

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+from jirafs import __version__ as version_string
+
+
+requirements_path = os.path.join(
+    os.path.dirname(__file__),
+    'requirements.txt',
+)
+try:
+    from pip.req import parse_requirements
+    requirements = [
+        str(req.req) for req in parse_requirements(requirements_path)
+    ]
+except ImportError:
+    requirements = []
+    with open(requirements_path, 'r') as in_:
+        requirements = [
+            req for req in in_.readlines()
+            if not req.startswith('-')
+            and not req.startswith('#')
+        ]
+
+
+setup(
+    name='jirafs',
+    version=version_string,
+    url='https://github.com/coddingtonbear/jirafs',
+    description=(
+        'Edit Jira issues as text files locally.'
+    ),
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=requirements,
+    packages=[
+        'jirafs',
+    ]
+)
