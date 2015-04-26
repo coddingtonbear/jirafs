aa2df42b0f158e9f19376cbdcf02c0a22f90c9e9
Adam Coddington <me@adamcoddington.net>
2015-04-26 14:33:55 -0700

    Adding an integration test covering that the expected files were created.

--- tests/integration/test_clone_issue.py
+++ tests/integration/test_clone_issue.py
@@ -2,9 +2,11 @@ import os
 
 from .base import IntegrationTestBase
 
+from jirafs import constants
+
 
 class TestCloneIssue(IntegrationTestBase):
-    def test_cloning_issue(self):
+    def test_cloning_issue_basic(self):
         issue_path = os.path.join(self.path, 'issue')
         data = {
             'description': 'This is a test description',
@@ -26,3 +28,42 @@ class TestCloneIssue(IntegrationTestBase):
         # Assert that the field data is on-disk as expected
         for key, value in data.items():
             self.assertEqual(value, fields.get(key))
+
+    def test_cloning_issue_files_exist(self):
+        issue_path = os.path.join(self.path, 'issue')
+        data = {
+            'description': 'This is a test description',
+            'summary': 'This is a test summary',
+        }
+
+        issue = self.create_issue(data)
+
+        self.run_command(
+            'clone',
+            path=issue_path,
+            url=issue.permalink(),
+            jira=self.get_jira
+        )
+
+        folder = self.get_ticket_folder_for_path(issue_path)
+
+        description_filename = constants.TICKET_FILE_FIELD_TEMPLATE.format(
+            field_name='description'
+        )
+        expected_files = [
+            constants.TICKET_DETAILS,
+            constants.TICKET_COMMENTS,
+            constants.TICKET_NEW_COMMENT,
+            constants.TICKET_LINKS,
+            description_filename
+        ]
+        all_files = os.listdir(issue_path)
+
+        for filename in expected_files:
+            self.assertIn(filename, all_files)
+
+        with open(folder.get_path(description_filename), 'r') as handle:
+            self.assertEqual(
+                data['description'],
+                handle.read().strip(),
+            )
