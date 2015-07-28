b3b40619002e6dec48bd6ee0744a868b823a7afb
Adam Coddington <me@adamcoddington.net>
2015-07-27 21:03:48 -0700

    Issue 28: Change expected field format to always include parenthesized actual field name.

--- tests/test_jirafieldmanager.py
+++ tests/test_jirafieldmanager.py
@@ -1,17 +1,15 @@
 from textwrap import dedent
 from unittest import TestCase
 
-from mock import patch
-
 from jirafs.jirafieldmanager import JiraFieldManager
 
 
 class TestJiraFieldManager(TestCase):
     def test_decode(self):
         encoded_values = dedent("""
-            * summary:
+            * summary (summary):
                 This is a test summary
-            * longer_message:
+            * Longer Message (longer_message):
                 This is a much
                 longer message that happens
                 to contain newlines.
@@ -37,15 +35,15 @@ class TestJiraFieldManager(TestCase):
 
     def test_decode_with_json_values(self):
         encoded_values = dedent("""
-            * dictionary_field:
+            * Dictionary Field (dictionary_field):
                 {
                     "name": "Adam Coddington"
                 }
-            * integer_field:
+            * Integer Field (integer_field):
                 10
-            * string_field:
+            * String Field (string_field):
                 Hello
-            * list_field:
+            * List Field (list_field):
                 [
                     "Alphabet",
                     {
@@ -76,37 +74,3 @@ class TestJiraFieldManager(TestCase):
             expected_result,
             actual_result,
         )
-
-    def test_decode_with_human_readable_field_names(self):
-        encoded_values = dedent("""
-            * Summary:
-                This is a test summary
-            * Longer Message:
-                This is a much
-                longer message that happens
-                to contain newlines.
-            * Something(s) that are Company-Specific (customfield_108234):
-                This is something else
-        """)
-
-        expected_result = {
-            'Summary': 'This is a test summary',
-            'Longer_Message': (
-                'This is a much\n'
-                'longer message that happens\n'
-                'to contain newlines.'
-            ),
-            'customfield_108234': (
-                'This is something else'
-            )
-        }
-
-        actual_result = JiraFieldManager(
-            encoded_values
-        )
-
-        self.assertEqual(
-            expected_result,
-            actual_result
-        )
-
