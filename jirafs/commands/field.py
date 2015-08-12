79f22e8dd2321c9ef30a4432fc575ec4970a1c85
Adam Coddington <me@adamcoddington.net>
2015-08-11 23:37:02 -0700

    Adds functionality for fetching JSON dotpaths of keys using the 'field' command; fixes #32.

--- jirafs/commands/field.py
+++ jirafs/commands/field.py
@@ -1,3 +1,5 @@
+import json
+
 from jirafs.exceptions import JirafsError
 from jirafs.plugin import CommandPlugin
 
@@ -9,7 +11,9 @@ class Command(CommandPlugin):
     MAX_VERSION = '1.99.99'
 
     def handle(self, args, folder, **kwargs):
-        return self.field(folder, args.field_name, raw=args.raw)
+        return self.field(
+            folder, args.field_name, raw=args.raw, formatted=args.formatted
+        )
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -22,16 +26,55 @@ class Command(CommandPlugin):
             default=False
         )
         parser.add_argument(
+            '--formatted',
+            help=(
+                'Format JSON output with indentation and sorted keys.'
+            ),
+            action='store_true',
+            default=False
+        )
+        parser.add_argument(
             'field_name',
         )
 
-    def field(self, folder, field_name, raw=False):
+    def field(self, folder, field_name, raw=False, formatted=False):
         fields = folder.get_fields()
+
+        key_dotpath = None
+        if '.' in field_name:
+            field_name, key_dotpath = field_name.split('.', 1)
 
         if field_name not in fields:
             raise JirafsError("Field '%s' does not exist." % field_name)
 
         if raw:
-            print(fields[field_name])
+            data = fields[field_name]
         else:
-            print(fields.get_transformed(field_name))
+            data = fields.get_transformed(field_name)
+
+        if key_dotpath:
+            try:
+                for component in key_dotpath.split('.'):
+                    if component not in data:
+                        data = None
+                        break
+                    data = data[component]
+            except (ValueError, TypeError):
+                raise JirafsError(
+                    "Field '%s' could not be parsed as JSON for retrieving "
+                    "dotpath '%s'." % (
+                        field_name,
+                        key_dotpath,
+                    )
+                )
+
+        if isinstance(data, (list, dict)):
+            kwargs = {}
+            if formatted:
+                kwargs = {
+                    'indent': 4,
+                    'sort_keys': True,
+                }
+            data = json.dumps(data, **kwargs)
+
+        print(data)
