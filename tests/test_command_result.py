24850b9eecfaec237a91b20ec6e4572d1532ad87
Adam Coddington <me@adamcoddington.net>
2015-10-10 23:30:44 -0700

    Issue 34: Adding test covering command result addition.

---
+++ tests/test_command_result.py
@@ -0,0 +1,26 @@
+from mock import patch
+
+from jirafs.plugin import CommandResult
+
+from .base import BaseTestCase
+
+
+class TestCommandResult(BaseTestCase):
+    def test_echo_cursor(self):
+        with patch.object(CommandResult, '_echo') as out:
+            lines = [
+                'Line 1',
+                'Line 2',
+            ]
+            result = CommandResult(lines[0]).echo()
+            result = result.add_line(lines[1]).echo()
+
+            self.assertTrue(len(out.call_args_list), 2)
+
+            for idx, line in enumerate(lines):
+                args, _ = out.call_args_list[idx]
+
+                self.assertEqual(
+                    args[0],
+                    line + '\n'
+                )
