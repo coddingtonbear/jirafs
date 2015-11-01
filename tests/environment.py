a6ce85fa26235769b84003cf3cd73999b343bee6
Adam Coddington <me@adamcoddington.net>
2015-11-01 12:22:56 -0800

    Make temp dirs for each scenario.

--- tests/environment.py
+++ tests/environment.py
@@ -3,12 +3,12 @@ import shutil
 import tempfile
 
 
-def before_all(context):
+def before_scenario(context, *args, **kwargs):
     context.starting_dir = os.getcwd()
     context.temp_dir = tempfile.mkdtemp()
     os.chdir(context.temp_dir)
 
 
-def after_all(context):
+def after_scenario(context, *args, **kwargs):
     os.chdir(context.starting_dir)
     shutil.rmtree(context.temp_dir)
