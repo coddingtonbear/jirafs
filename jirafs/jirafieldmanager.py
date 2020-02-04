1f9f4a2242278b21b6416c779f66276f217e9227
Adam Coddington <me@adamcoddington.net>
2020-02-04 06:41:43 -0800

    Fixes bug preventing default value from being returned from jirafs.jirafieldmanager.JiraFieldManager.get_transformed should a KeyError be raised for a non-separated field.  Fixes #59.

--- jirafs/jirafieldmanager.py
+++ jirafs/jirafieldmanager.py
@@ -54,10 +54,10 @@ class JiraFieldManager(dict):
                 yield k, v
 
     def get_transformed(self, field_name, default=None):
-        if field_name not in self.get_requested_per_ticket_fields():
-            return self[field_name]
-
         try:
+            if field_name not in self.get_requested_per_ticket_fields():
+                return self[field_name]
+
             return self.folder.process_macros(
                 self[field_name], path=self.get_generated_file_path()
             )
